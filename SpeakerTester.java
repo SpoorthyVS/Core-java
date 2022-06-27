a59a8fbf0c4c96ab3ed1661bf8df582e7f5e7dc1
SpoorthyVS <spoorthyvsoma.xworkz@gmail.com>
2022-06-27 19:08:05 +0530

    commiting methods file

---
+++ SpeakerTester.java
@@ -0,0 +1,28 @@
+class SpeakerTester{
+
+		public static void main(String a[]){
+		/**Speaker speaker = new Speaker("Noise");
+		boolean isConnected = speaker.onOrOff();
+		System.out.println(isConnected);
+		boolean isConnected1 = speaker.onOrOff();
+		System.out.println(isConnected1);
+		boolean isConnected2 = speaker.onOrOff();
+		System.out.println(isConnected2);
+		boolean isConnected3 = speaker.onOrOff();
+		System.out.println(isConnected3);*/
+		
+		Speaker.onOrOff();
+		Speaker.increaseVolume();
+		Speaker.increaseVolume();
+		Speaker.increaseVolume();
+		Speaker.increaseVolume();
+		Speaker.increaseVolume();
+		Speaker.increaseVolume();
+		Speaker.onOrOff();
+		Speaker.onOrOff();
+		Speaker.onOrOff();
+		Speaker.increaseVolume();
+		Speaker.increaseVolume();
+		Speaker.increaseVolume();
+	}
+}
