5dd4f87e1d7db870cf1c4a5153a74394bb2cd798
SpoorthyVS <spoorthyvsoma.xworkz@gmail.com>
2022-07-25 13:36:35 +0530

    commiting eclipes abstraction files

---
+++ Airport-management-system/src/com/xworkz/Airport/dto/TerminalDTO.java
@@ -0,0 +1,16 @@
+package com.xworkz.Airport.dto;
+
+import lombok.Data;
+
+@Data
+public class TerminalDTO {
+	 private int id;
+	 private String name;
+	 private int noOfLoadingBridges;
+	 private int noOfDepartureGates;
+	 private int noOfCheckIncounters;
+	 
+	
+	 
+	 
+}
